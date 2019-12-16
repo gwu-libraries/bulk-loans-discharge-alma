64f65881407d57e43819b4b378641881cf8d4966
Dolsy Smith <dsmith@gwu.edu>
2019-12-16 14:56:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+This script uses Alma's (Scan-In API)[https://developers.exlibrisgroup.com/alma/apis/docs/bibs/UE9TVCAvYWxtYXdzL3YxL2JpYnMve21tc19pZH0vaG9sZGluZ3Mve2hvbGRpbmdfaWR9L2l0ZW1zL3tpdGVtX3BpZH0=/] to discharge loans from a CSV file containing the relevant system identifiers. The CSV can be generated from an Analytics report, and it should include columns for MMS Id, Holdings Id, and Item Id, the owning library code, and either a temporary or permanent location code.
+
+An additional CSV is required to map each location code (temporary or permanent) to a circulation desk that is configured for discharges at that location. 
+
+** The script does not currently handle discharging items from a mix of temporary and permanent locations. If this is desired, break the list of items into two sets: one containing those items in temporary locations, the other containing those items in their permanent locations.**
+
+The script uses asynchronous methods to optimize API calls; up to 25 items can be scanned in per second. A column called "done" is added to the CSV data source in order to indicate those rows that have successfully been scanned in. 
+
+In addition, JSON results from the API are stored to disk for use in troubleshooting items with errors. Results are returned in batches of 1000 items.
+
+###Requirements
+
+pyyaml==5.1.1
+pandas==0.24.2
+aiohttp==3.5.4
+
+Tested on Python 3.7
+
+###Setup
+
+1. Configure an API key for the Alma Bibs API with read/write access.
+2. Download or clone the contents of this repo.
+3. In Analytics, copy both reports in the folder `Shared Folders/Community/Reports/Consortia/WRLC/Loans Discharge App` to your own folder. ** Please don't edit the shared versions of these reports. **
+4. Configure the report called `loans_by_user_group` to retrieve the items on loan in your IZ that you wish to discharge.
+5. Run the Analytics report `items_to_discharge`. This report depends on the report from step 4 but contains the necessary system Id's for use with the Bibs API. Export this report as a CSV file.
+6. Edit the `locations_to_circ.csv` file (or create a new one) mapping each (permanent or temporary) item location to a circulation desk configured to discharge items from that location. ** This is necessary to ensure that discharged items do not generate reshelving requests in Alma. **
+7. Edit the file `_loan_discharge_config.yml`:
+  - Update the location of the CSV files from steps 5 and 6 (if they are not located in the same directory as the scripts). 
+  - Paste your API key from step 1 into the `apikey` field.
+  - Make sure the value of the `location_type` key matches the kind of location associated with the items to be discharged. Possible values are `temp` and `perm`.
+  - Change the name of the file to `loan_discharge_config.yml` (removing the leading underscore).
+8. Create a subdirectory called `data` in the directory that houses the scripts. The JSON returned by the API will be saved to this directory. 
+9. Run the script `discharge_loans.py` from the command line: e.g., `python discharge_loans.py`
---
+++ discharge_loans.py
@@ -0,0 +1,159 @@
+import yaml
+import pandas as pd
+from async_fetch import run_batch
+import asyncio
+from pathlib import Path
+from datetime import datetime
+
+# Bibs API endpoint for scanning in items
+SCAN_URL = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_id}'
+# Basic header for POST operation to scan endpoint
+headers = {'Content-Type': 'application/json',
+		   'Accept': 'application/json'}
+# Basic parameters for use with POST op to scan endpoint
+scan_params = {'op': 'scan',
+			  'done': 'true'}
+
+def load_loan_data(path_to_loans):
+	'''Loads a CSV of items for discharge. CSV should include columns MMS Id, Holding Id, and Item Id.'''
+	try:
+		data = pd.read_csv(path_to_loans)
+		return data
+	except FileNotFoundError:
+		print("Invalid path. loan-discharge-config.yml should include a path to a CSV file of item ID's for discharge.")
+		return None 
+
+def load_circ_desks(path_to_circ_desks):
+	'''Loads a CSV mapping Alma temporary locations to a circ desks for reshelving. Should include columns  location_code and circ_desk.'''
+	try:
+		desks = pd.read_csv(path_to_circ_desks)
+		return desks
+	except FileNotFoundError:
+		print("Invalid path. loan-discharge-config.yml should include a path to a CSV file mapping Alma temporary locations to their circulation desks for reshelving.")
+		return None 
+
+def convert_header(header):
+	'''Helper function for converting Analytics report headers to a format for use in the Alma Bibs API.'''
+	return header.lower().replace(' ', '_')
+
+def prep_data(config):
+	'''Loads and preps data for batch processing.'''
+	items = load_loan_data(config['loans_data_file'])
+	desks = load_circ_desks(config['circ_desks_file'])
+	if items.empty or desks.empty:
+		return None
+	# Convert Analytics column names
+	items.columns = [convert_header(c) for c in items.columns]
+	# Add circ_desk column, matching on a location code
+	# The config file should specify whether temp or perm
+	if config['location_type'] == 'temp':
+		location_column = 'temporary_location_code'
+	elif config['location_type'] == 'perm':
+		location_column = 'location_code'
+	else:
+		print('Invalid location_type provided in the config file. Possible values are "temp" and "perm."')
+		return None
+	try:
+		items = items.merge(desks, 
+						left_on=location_column,
+						right_on='location_code')
+	except KeyError:
+		print('Invalid columns for matching each temporary location to its circ desk. The loans_data_file should have either a temporary_location_code or location_code column; the circ_desks_file should have a location_code column (which holds the temporary location code associated with each circ desk).')
+		return None
+	# Validate and convert DataFrame to list of records for feeding to the API in sequence
+	# Return both the DF and the rows; the DF will be used to flag completed items
+	if validate_data(items):
+		return items
+	return None
+
+def validate_data(items):
+	'''Checks to make sure the required columns are present for the data elements used by the API.
+	Argument should be a DataFrame.'''
+	try:
+		assert {'mms_id', 'holding_id', 'item_id', 'circ_desk', 'library_code'}.issubset(items.columns)
+		return True
+	except AssertionError:
+		print('Missing columns. Data set for discharge must include MMS Id, Holding Id, Item Id, Circ Desk, and Library Code columns.')
+		return None
+
+
+def extract_ids(response):
+	'''Helper function to extract the system ID's from the API responses -- useful for tracking what we have left to do.
+	Argument should be a JSON response from the Scan-In API.'''
+	try:
+		mms_id = response['bib_data']['mms_id']
+		holding_id = response['holding_data']['holding_id']
+		item_id = response['item_data']['pid']
+		return {'mms_id': mms_id,
+				'holding_id': holding_id,
+			   'item_id': item_id}
+	except Exception as e:
+		return None
+
+def compute_remainder(results, data, test_fn):
+	'''Given a partial list of results, identify those on the original dataset as completed.
+	results should be a list of results from the run_batch function.
+	data should be the original data set (as a DataFrame).
+	test_fn should be a function that returns either a valid row in the data set or else None.'''
+	completed = [test_fn(r['response']) for r in results]
+	completed = pd.DataFrame.from_records([c for c in completed if c])
+	if completed.empty:
+		print("No rows could be completed. Please see results_batch.json for errors.")
+		return pd.DataFrame()
+	# Convert Id's from strings to int
+	for c in completed.columns:
+		completed[c] = completed[c].astype(int)
+	# Add column marking the completed results
+	completed['done'] = True
+	return data.merge(completed, on=['mms_id', 
+									'holding_id',
+									'item_id'],
+									how='left')
+
+def param_fn(row):
+	'''Function to generate params for the scan-in API for each row in the data.. 
+		Updates the desk for discharge depending on the item's location.
+		In this case, data to POST will be None.'''
+	scan_params['circ_desk'] = row['circ_desk']
+	scan_params['library'] = row['library_code']
+	return scan_params, None
+
+def do_scan_in(rows, config):
+	'''Perform scan-in (using the run_batch function from async_fetch), accumulating results. '''
+	headers['Authorization'] = 'apikey {}'.format(config['apikey'])
+	loop = asyncio.get_event_loop()
+	results = []
+	print("Scanning in items...")
+	for batched_result in run_batch(loop,
+								rows,
+								param_fn=param_fn,
+								base_url=SCAN_URL,
+								headers=headers,
+								path_to_files=config['data_dir'],
+								batch_size=1000,
+								http_type='POST'):
+		results.extend(batched_result)
+	return results
+
+def discharge_loans():
+	'''Main function for running batch discharge.'''
+	# Loading the config objects from YAML
+	with open('./loan_discharge_config.yml', 'r') as f:
+		config = yaml.load(f, Loader=yaml.FullLoader)
+	data = prep_data(config)
+	if data.empty:
+		return None
+	rows = [d._asdict() for d in data.itertuples(index=False)]
+	results = do_scan_in(rows, config)
+	print("Scan-in complete. Marking data set for successful discharges...")
+	# Merge completed results with original data set
+	data = compute_remainder(results=results, data=data, test_fn=extract_ids)
+	if data.empty:
+		return
+	# Rename the file for clarity, adding today's date
+	out_path = Path(config['loans_data_file'])
+	new_file = out_path.stem + '_{}.csv'.format(datetime.now().strftime('%Y-%m-%d'))
+	data.to_csv(out_path.parent / new_file, index=False)
+
+if __name__ == '__main__':
+	discharge_loans()
